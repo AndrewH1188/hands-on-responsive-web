6fcee53b17215890c17bc2b16c06213fb0cc656a
AndrewH1188 <andrewh1188@gmail.com>
2022-12-09 15:53:08 +0000

    Added and styled the Active state for larger devices as well as enabled the show and hide to work.

--- js/hideMenu.js
+++ js/hideMenu.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-    $(function() {
-        $('#showPhoneNav').click(function() {
-            $("#myNav").slideToggle('normal', function(){ //
-                // If a menu is showing - hide it. If the menu is hiding - show it
+$(function() {
+    $('#showPhoneNav').click(function() {
+        $("#myNav").slideToggle('normal',function(){  // Shows Nav Area
             if ($('#myNav').is(':visible')) {
-                $('showPhoneNav').text('Hide Navigation');
+                $('#showPhoneNav').text('Hide Navigation');
             } else {
                 $('#showPhoneNav').text('Show Navigation');
             } // end of if
-        }); // end of slidetoggle
-            }); // end of myNav
-        }); //end showPhoneNav
-    }); // end of ready
+            }); // end of slidetoggle
+    }); // end of myNav
+}); //end showPhoneNav
+}); // end of ready
